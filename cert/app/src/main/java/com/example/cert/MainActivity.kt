04a78aaf0025098e161beb0c14ca6f6f7dc7aad2
choi98-git <79885792+choi98-git@users.noreply.github.com>
2021-07-15 17:55:53 +0800

    Main화면 수정 및 D-Day 기능 추가
    Main화면에 D-Day 설정 버튼 추가
    D-Day 설정을 위한 CalenderView Activity 추가 날짜 계산 기능 구현

--- cert/app/src/main/java/com/example/cert/MainActivity.kt
+++ cert/app/src/main/java/com/example/cert/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.cert
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dDayButton: Button = findViewById(R.id.dDaySetting)
+        val dDayText = findViewById<TextView>(R.id.dDay)
+
+        //  intent를 통해 넘어온 문자열을 difDate 변수에 담음
+        val difDate = intent.getStringExtra("difDate")
+
+        dDayText.text = difDate
+
+        // D-Day설정 버튼을 눌러 설정창으로 이동
+        dDayButton.setOnClickListener {
+            val intent = Intent(this, DdayActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
